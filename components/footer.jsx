7b460383ad4e28f2a62c2bea53ef3d430d33cea3
advay-c <advay1612@icloud.com>
2024-01-18 00:28:12 -0500

    Remove unused import in footer component

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,10 +1,8 @@
 import React from "react";
-import { LuGithub } from "react-icons/lu";
 
 const Footer = () => {
   return (
     <div className="footer-container">
-    <LuGithub />
     </div>
   );
 };
