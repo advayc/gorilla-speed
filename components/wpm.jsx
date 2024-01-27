5cca9ebf51f2e9f3587247de947bccc1acce845b
advay-c <advay1612@icloud.com>
2024-01-27 13:10:26 -0500

    Stop the timer when typing is completed

--- components/wpm.jsx
+++ components/wpm.jsx
@@ -7,38 +7,42 @@ const WPM = () => {
   const [startTime, setStartTime] = useState(null);
   const [endTime, setEndTime] = useState(null);
   const [isTyping, setIsTyping] = useState(false);
+  const [isComplete, setIsComplete] = useState(false);
   const inputRef = useRef(null);
+  const placeholderCount = 30;
 
   useEffect(() => {
-    const words = [ 'the', 'be', 'to', 'of', 'and', 'a', 'in', 'that', 'have', 'I', 'it', 'for', 'not', 'on', 'with', 
-      'will', 'my', 'one', 'all', 'would', 'there', 'their', 'what', 'so', 'up', 'out', 'if', 'about', 'who', 'get', 'which', 
-      'good', 'some', 'could', 'them', 'see', 'other', 'than', 'then', 'now', 'look', 'only', 'come', 'its', 'over', 'think', 
-      'also', 'back', 'after', 'use', 'two', 'how', 'our', 'work', 'first', 'well', 'way', 'even', 'new', 'want', 'because', 
-      'any', 'these', 'give', 'day', 'most', 'us', 'way', 'off', 'here', 'why', 'went', 'many', 'where', 'under', 'back', 
-      'little', 'round', 'man', 'came', 'show', 'every', 'me', 'name', 'very', 'through', 'form', 'much', 'great', 'help', 
-      'low', 'line', 'before', 'turn', 'cause', 'same', 'mean', 'differ', 'move', 'right', 'boy', 'old', 'too', 'does', 'tell', 
-      'sentence', 'set', 'three', 'air', 'play', 'small', 'end', 'put', 'home', 'read', 'hand', 'port', 'large', 'spell', 'add', 
-      'land', 'must', 'big', 'high', 'such', 'follow', 'act', 'ask', 'men', 'change', 'light', 'kind', 'off', 'need', 'house', 
-      'picture', 'try', 'again', 'animal', 'point', 'mother', 'world', 'near', 'build', 'self', 'earth', 'father', 'head', 
-      'stand', 'own', 'page', 'should', 'country', 'found', 'answer', 'school', 'grow', 'study', 'still', 'learn', 'plant', 
-      'cover', 'food', 'sun', 'four', 'thought', 'let', 'keep', 'eye', 'never', 'last', 'door', 'between', 'city', 'tree', 'cross', 
-      'since', 'hard', 'start', 'might', 'story', 'saw', 'far', 'sea', 'draw', 'left', 'late', 'run', "don't", 'while', 'press', 
-      'close', 'night', 'real', 'life', 'few', 'north', 'open', 'seem', 'together', 'next', 'white', 'children', 'begin', 'got', 
-      'walk', 'example', 'ease', 'paper', 'group', 'always', 'music', 'those', 'both', 'mark', 'book', 'letter', 'until', 'mile', 
-      'river', 'car', 'feet', 'care', 'second', 'enough', 'plain', 'girl', 'usual', 'young', 'ready', 'above', 'ever', 'red', 
-      'list', 'though', 'feel', 'talk', 'bird', 'soon', 'body', 'dog', 'family', 'direct', 'pose', 'leave', 'song', 'measure', 
-      'product', 'black', 'short', 'numeral', 'class', 'wind', 'sit', 'question', 'happen', 'complete', 'ship', 'area', 'half', 
-      'rock', 'order', 'fire', 'south', 'problem', 'piece', 'told', 'knew', 'pass', 'top', 'whole', 'king', 'street', 'inch', 
-      'multiply', 'nothing', 'course', 'stay', 'wheel', 'full', 'force', 'blue', 'object', 'decide', 'surface', 'deep', 'moon', 
-      'island', 'foot', 'system', 'busy', 'test', 'record', 'boat', 'common', 'gold', 'possible', 'plane', 'stead', 'dry', 
-      'wonder', 'laugh', 'thousand', 'ago', 'ran', 'check', 'game', 'shape', 'yes', 'hot', 'miss', 'brought', 'heat', 'snow', 
-      'bed', 'bring', 'sit', 'perhaps', 'fill', 'east', 'weight', 'language', 'among', 'fine', 'ball', 'yet', 'wave', 'drop', 
-      'heart', 'am', 'present', 'heavy', 'dance', 
-      'he', 'as', 'you', 'do', 'at', 'this', 'but', 'his', 'by', 'from', 'they', 'we', 'say', 'her', 'she', 'or', 'an', 
-      'go', 'me', 'when', 'make', 'can', 'like', 'time', 'no', 'just', 'him', 'know', 'take', 'people', 'into', 'year', 'your', 
+    const words = [
+      'the', 'be', 'to', 'of', 'and', 'a', 'in', 'that', 'have', 'I', 'it', 'for', 'not', 'on', 'with',
+      'will', 'my', 'one', 'all', 'would', 'there', 'their', 'what', 'so', 'up', 'out', 'if', 'about', 'who', 'get', 'which',
+      'good', 'some', 'could', 'them', 'see', 'other', 'than', 'then', 'now', 'look', 'only', 'come', 'its', 'over', 'think',
+      'also', 'back', 'after', 'use', 'two', 'how', 'our', 'work', 'first', 'well', 'way', 'even', 'new', 'want', 'because',
+      'any', 'these', 'give', 'day', 'most', 'us', 'way', 'off', 'here', 'why', 'went', 'many', 'where', 'under', 'back',
+      'little', 'round', 'man', 'came', 'show', 'every', 'me', 'name', 'very', 'through', 'form', 'much', 'great', 'help',
+      'low', 'line', 'before', 'turn', 'cause', 'same', 'mean', 'differ', 'move', 'right', 'boy', 'old', 'too', 'does', 'tell',
+      'sentence', 'set', 'three', 'air', 'play', 'small', 'end', 'put', 'home', 'read', 'hand', 'port', 'large', 'spell', 'add',
+      'land', 'must', 'big', 'high', 'such', 'follow', 'act', 'ask', 'men', 'change', 'light', 'kind', 'off', 'need', 'house',
+      'picture', 'try', 'again', 'animal', 'point', 'mother', 'world', 'near', 'build', 'self', 'earth', 'father', 'head',
+      'stand', 'own', 'page', 'should', 'country', 'found', 'answer', 'school', 'grow', 'study', 'still', 'learn', 'plant',
+      'cover', 'food', 'sun', 'four', 'thought', 'let', 'keep', 'eye', 'never', 'last', 'door', 'between', 'city', 'tree', 'cross',
+      'since', 'hard', 'start', 'might', 'story', 'saw', 'far', 'sea', 'draw', 'left', 'late', 'run', "don't", 'while', 'press',
+      'close', 'night', 'real', 'life', 'few', 'north', 'open', 'seem', 'together', 'next', 'white', 'children', 'begin', 'got',
+      'walk', 'example', 'ease', 'paper', 'group', 'always', 'music', 'those', 'both', 'mark', 'book', 'letter', 'until', 'mile',
+      'river', 'car', 'feet', 'care', 'second', 'enough', 'plain', 'girl', 'usual', 'young', 'ready', 'above', 'ever', 'red',
+      'list', 'though', 'feel', 'talk', 'bird', 'soon', 'body', 'dog', 'family', 'direct', 'pose', 'leave', 'song', 'measure',
+      'product', 'black', 'short', 'numeral', 'class', 'wind', 'sit', 'question', 'happen', 'complete', 'ship', 'area', 'half',
+      'rock', 'order', 'fire', 'south', 'problem', 'piece', 'told', 'knew', 'pass', 'top', 'whole', 'king', 'street', 'inch',
+      'multiply', 'nothing', 'course', 'stay', 'wheel', 'full', 'force', 'blue', 'object', 'decide', 'surface', 'deep', 'moon',
+      'island', 'foot', 'system', 'busy', 'test', 'record', 'boat', 'common', 'gold', 'possible', 'plane', 'stead', 'dry',
+      'wonder', 'laugh', 'thousand', 'ago', 'ran', 'check', 'game', 'shape', 'yes', 'hot', 'miss', 'brought', 'heat', 'snow',
+      'bed', 'bring', 'sit', 'perhaps', 'fill', 'east', 'weight', 'language', 'among', 'fine', 'ball', 'yet', 'wave', 'drop',
+      'heart', 'am', 'present', 'heavy', 'dance',
+      'he', 'as', 'you', 'do', 'at', 'this', 'but', 'his', 'by', 'from', 'they', 'we', 'say', 'her', 'she', 'or', 'an',
+      'go', 'me', 'when', 'make', 'can', 'like', 'time', 'no', 'just', 'him', 'know', 'take', 'people', 'into', 'year', 'your',
     ];
+
     const randomWords = [];
-    for (let i = 0; i < 30; i++) {
+    for (let i = 0; i < placeholderCount; i++) {
       const randomIndex = Math.floor(Math.random() * words.length);
       randomWords.push(words[randomIndex]);
     }
@@ -48,6 +52,16 @@ const WPM = () => {
 
     inputRef.current.focus();
   }, []);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (isTyping && inputValue !== placeholder && !isComplete) {
+        setEndTime(Date.now());
+      }
+    }, 100); // Update the timer every 100 milliseconds
+
+    return () => clearInterval(interval);
+  }, [isTyping, inputValue, placeholder, isComplete]);
 
   const handleInputChange = (event) => {
     const { value } = event.target;
@@ -59,41 +73,45 @@ const WPM = () => {
     }
 
     if (value === placeholder) {
-      setEndTime(Date.now());
+      setIsTyping(false); // Stop the timer when typing is completed
+      setIsComplete(true); // Set flag to indicate typing completion
+    }
+
+    if (value.length > placeholder.length) {
+      setIsComplete(false); // Reset the flag if the user deletes characters
     }
   };
 
   useEffect(() => {
-    if (startTime && endTime) {
+    if (startTime && endTime && !isComplete) {
       console.log(`Time taken: ${endTime - startTime} milliseconds`);
     }
-  }, [startTime, endTime]);
+  }, [startTime, endTime, isComplete]);
 
-  const timeElapsed = isTyping ? ((Date.now() - (startTime || Date.now())) / 1000).toFixed(2) : 0;
-  const toFixed = (num) => (parseFloat(num).toFixed(2));
+  const timeElapsed = ((Date.now() - (startTime || Date.now())) / 1000).toFixed(2);
 
   return (
     <div>
       <h2 className={styles.subtitle}>Test Your typing Speed!</h2>
       <div className={styles.timer}>
-            Timer
-      <h3 className={styles.timernum}>{toFixed(timeElapsed)}</h3>
+        Timer
+        <h3 className={styles.timernum}>{timeElapsed}</h3>
       </div>
       <div className={styles.textarea}>
-      <input
+        <input
           id="textInput"
           value={inputValue}
           onChange={handleInputChange}
           className={styles.input}
           autoFocus
-          ref={inputRef} 
+          ref={inputRef}
         />
         <div className={styles.placeholder}>
           {placeholder.split('').map((char, index) => (
             <span
               key={index}
               style={{
-                color: inputValue[index] === char ? 'white' : inputValue[index] !== undefined ? 'red' : 'grey',
+                color: (isComplete && index < inputValue.length) ? 'white' : inputValue[index] === char ? 'white' : inputValue[index] !== undefined ? 'red' : 'grey',
               }}
             >
               {char}
