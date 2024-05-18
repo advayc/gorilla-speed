bcb7007ed23b87caca2ea869d892ec1208f79612
advay-c <advay1612@icloud.com>
2024-05-18 19:42:14 -0400

    responsive

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,32 +1,29 @@
 import React from 'react';
 import Navbar from '@/components/navbar.jsx';
 import styles from '@/styles/Home.module.css';
-import AnimatedCursor from "react-animated-cursor"
-import { Analytics } from "@vercel/analytics/react"
+import AnimatedCursor from 'react-animated-cursor';
+import { Analytics } from '@vercel/analytics/react';
+
 export default function Home() {
-
   return (
-      <main className={styles.main}>
+    <main className={styles.main}>
       <AnimatedCursor
-      innerSize={0.1}
-      outerSize={30}
-      color='224, 224, 224'
-      outerAlpha={0.2}
-      outerScale={2}
-      trailingSpeed={5}
-      showSystemCursor='true'
-    />
+        innerSize={0.1}
+        outerSize={30}
+        color='224, 224, 224'
+        outerAlpha={0.2}
+        outerScale={2}
+        trailingSpeed={5}
+        showSystemCursor='true'
+      />
       <Navbar />
-        <div className={`${styles.container} ${styles.fadeIn}`}>
-          <h1 className={styles.title}>
-            Welcome to Gorilla Speed
-          </h1>
-          <h5 className={styles.description}>
-            The best way to test your typing and clicking speed
-          </h5>
-        </div>
-        <Analytics />
-
-      </main>
-  )
+      <div className={`${styles.container} ${styles.fadeIn}`}>
+        <h1 className={styles.title}>Welcome to Gorilla Speed</h1>
+        <h5 className={styles.description}>
+          The best way to test your typing and clicking speed
+        </h5>
+      </div>
+      <Analytics />
+    </main>
+  );
 }
